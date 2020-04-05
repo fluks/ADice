cb1a4ef9af364aa14dd73e07c49dbfe81a20c8f7
fluks <fluks.github@gmail.com>
2020-04-05 13:30:30 +0300

    Delete settings item from settings toolbar

--- app/src/main/java/fi/jukkalaitinen/adice/MainActivity.kt
+++ app/src/main/java/fi/jukkalaitinen/adice/MainActivity.kt
@@ -69,7 +69,6 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> { true }
             R.id.sound_setting -> {
                 setSound(!item.isChecked)
                 item.setChecked(!item.isChecked)
